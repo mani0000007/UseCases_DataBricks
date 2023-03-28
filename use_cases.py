3b61ee09eb3177feeb4399f33aa16a0035b823e0
mani0000007 <124129703+mani0000007@users.noreply.github.com>
2023-03-28 10:15:39 +0530

    Final Version

--- use_cases.py
+++ use_cases.py
@@ -83,82 +83,82 @@ df = spark.read.format('com.crealytics.spark.excel') \
 df.write.format("delta").saveAsTable("Categories")
 
 
-##########   Top 10 products in each Category  ############
+1. ##########   Top 10 products in each Category  ############
 
 
-%sql
-SELECT c.CategoryID,c.CategoryName, p.ProductName, p.UnitPrice
-FROM Categories c
-INNER JOIN (
-    SELECT CategoryID, ProductName, UnitPrice,
-           ROW_NUMBER() OVER (PARTITION BY CategoryID ORDER BY UnitPrice DESC) AS rank
-    FROM Products
-) p ON c.CategoryID = p.CategoryID AND p.rank <= 10
-ORDER BY c.CategoryName, p.UnitPrice DESC;
+        %sql
+        SELECT c.CategoryID,c.CategoryName, p.ProductName, p.UnitPrice
+        FROM Categories c
+        INNER JOIN (
+            SELECT CategoryID, ProductName, UnitPrice,
+                   ROW_NUMBER() OVER (PARTITION BY CategoryID ORDER BY UnitPrice DESC) AS rank
+            FROM Products
+        ) p ON c.CategoryID = p.CategoryID AND p.rank <= 10
+        ORDER BY c.CategoryName, p.UnitPrice DESC;
 
-#######----Top 10 Customers By sales In Each Country------#######
+2. #######----Top 10 Customers By sales In Each Country------#######
 
-%sql 
-drop table if exists salesCustomer;
-create table salesCustomer as select c.Country, o.CustomerID, SUM(od.UnitPrice * od.Quantity) AS TotalSales FROM Orders o JOIN OrderDetails od ON o.OrderID = od.OrderID JOIN Customers c ON o.CustomerID = c.CustomerID GROUP BY c.Country,o.CustomerID
+        %sql 
+        drop table if exists salesCustomer;
+        create table salesCustomer as select c.Country, o.CustomerID, SUM(od.UnitPrice * od.Quantity) AS TotalSales FROM Orders o JOIN OrderDetails od ON o.OrderID = od.OrderID JOIN Customers c ON o.CustomerID = c.CustomerID GROUP BY c.Country,o.CustomerID
 
 
-#########-----Top 10 products in each Category------####
+3. #########-----Top 10 products in each Category------####
 
-%sql
-SELECT c.CategoryID,c.CategoryName, p.ProductName, p.UnitPrice
-FROM Categories c
-INNER JOIN (
-    SELECT CategoryID, ProductName, UnitPrice,
-           ROW_NUMBER() OVER (PARTITION BY CategoryID ORDER BY UnitPrice DESC) AS rank
-    FROM Products
-) p ON c.CategoryID = p.CategoryID AND p.rank <= 10
-ORDER BY c.CategoryName, p.UnitPrice DESC;
+        %sql
+        SELECT c.CategoryID,c.CategoryName, p.ProductName, p.UnitPrice
+        FROM Categories c
+        INNER JOIN (
+            SELECT CategoryID, ProductName, UnitPrice,
+                   ROW_NUMBER() OVER (PARTITION BY CategoryID ORDER BY UnitPrice DESC) AS rank
+            FROM Products
+        ) p ON c.CategoryID = p.CategoryID AND p.rank <= 10
+        ORDER BY c.CategoryName, p.UnitPrice DESC;
 
--->
-drop table if exists salesCustCountry;
-create table salesCustcountry AS (SELECT Country, CustomerID, TotalSales, RANK() OVER (PARTITION BY Country ORDER BY TotalSales DESC) AS Rank
-FROM salesCustomer);
+        -->
+        drop table if exists salesCustCountry;
+        create table salesCustcountry AS (SELECT Country, CustomerID, TotalSales, RANK() OVER (PARTITION BY Country ORDER BY TotalSales DESC) AS Rank
+        FROM salesCustomer);
 
--->
-SELECT Country, CustomerID, TotalSales FROM salesCustCountry WHERE Rank <= 10 ORDER BY Country, TotalSales DESC;
+        -->
+        SELECT Country, CustomerID, TotalSales FROM salesCustCountry WHERE Rank <= 10 ORDER BY Country, TotalSales DESC;
 
-##########-----Country and Year wise sales of Sub-Total-----######
+4. ##########-----Country and Year wise sales of Sub-Total-----######
 
-%sql
-SELECT 
-  coalesce(Country, 'All') AS Country, 
-  year(cast(OrderDate as date)) AS Year, 
-  sum((Quantity * UnitPrice)) AS SubTotal 
-FROM 
-  (
-    SELECT 
-      c.Country, 
-      o.OrderDate, 
-      od.Quantity, 
-      od.UnitPrice 
-    FROM 
-      Customers c 
-      INNER JOIN Orders o ON c.CustomerID = o.CustomerID 
-      INNER JOIN OrderDetails od ON o.OrderID = od.OrderID
-  ) AS Sales 
-GROUP BY 
-  Country, 
-  Year 
-ORDER BY 
-  Country ASC NULLS LAST, 
-  Year ASC NULLS LAST;
+        %sql
+        SELECT 
+          coalesce(Country, 'All') AS Country, 
+          year(cast(OrderDate as date)) AS Year, 
+          sum((Quantity * UnitPrice)) AS SubTotal 
+        FROM 
+          (
+            SELECT 
+              c.Country, 
+              o.OrderDate, 
+              od.Quantity, 
+              od.UnitPrice 
+            FROM 
+              Customers c 
+              INNER JOIN Orders o ON c.CustomerID = o.CustomerID 
+              INNER JOIN OrderDetails od ON o.OrderID = od.OrderID
+          ) AS Sales 
+        GROUP BY 
+          Country, 
+          Year 
+        ORDER BY 
+          Country ASC NULLS LAST, 
+          Year ASC NULLS LAST;
   
  
- #######----------Common customers In Last Two Years -------#######
+5. #######----------Common customers In Last Two Years -------#######
  
- %sql
-create table maxyear as select * from Orders where year(OrderDate)=(select year(max(orderdate)) from Orders);
+         %sql
+        create table maxyear as select * from Orders where year(OrderDate)=(select year(max(orderdate)) from Orders);
 
-CREATE TABLE max1 AS
-SELECT *
-FROM Orders
-WHERE YEAR(OrderDate) = (SELECT YEAR(MAX(OrderDate)) FROM Orders) - 1;
-  
-select distinct(m.customerID) from maxyear m join max1 m2 on m.customerID=m2.customerID
-  
+        CREATE TABLE max1 AS
+        SELECT *
+        FROM Orders
+        WHERE YEAR(OrderDate) = (SELECT YEAR(MAX(OrderDate)) FROM Orders) - 1;
+
+        select distinct(m.customerID) from maxyear m join max1 m2 on m.customerID=m2.customerID
+
